132e49c54e2c0229a4000c01bee770afdb5bdc52
NLitissia <litissia.negaa@gmail.com>
2020-11-19 14:47:16 +0100

    heritage

---
+++ heritage.php
@@ -0,0 +1,41 @@
+<?php
+
+class Card{
+    /*Avec un attribut privé les class fille peuvent pas accéder
+      pour cela on peut utiliser protected
+    */
+    protected $_name;
+    protected $_description;
+
+    public function __construct($name,$description)
+    {
+        $this->_name = $name;
+        $this->_description = $description;
+    }
+    public function Hello(){
+     echo 'je suis la carte '.$this->_name;
+    }
+}
+
+class trapCard extends Card{
+    private $_effet;
+    public function __construct($name,$description,$effet)
+    { 
+        //appel le contructeur de la class mere 
+        parent::__construct($name,$description);
+        $this->_effet = $effet;
+        
+    }
+    public function Hello(){
+        echo 'je suis TrapCard';
+    }
+
+}
+
+/*$card = new Card('liti','description card');
+$card->Hello();*/
+
+$card = new TrapCard('liti','description card');
+$card->Hello();
+
+?>
